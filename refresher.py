b3a82a7956aad5689a7ba0e65fbe21d8e21c8681
1 <ressivage@ya.ru>
2022-08-06 13:03:41 +0500

    вроде поправил обновление базы

--- refresher.py
+++ refresher.py
@@ -7,7 +7,6 @@ from rq import Worker, Queue, Connection
 
 def bd_refresh():
     con = sqlite3.connect("lab2.db")
-    print(os.path.abspath('lab2.db'))
     cur = con.cursor()
     print("iteration")
     data = grab_current()
@@ -24,12 +23,11 @@ def bd_refresh():
         {data["humidity"]},
         '{datetime.datetime.now().replace(microsecond=0)}')
         """)
-    print(sql)
+    print(cur.rowcount)
     result = cur.execute(sql)
-    
-    print(os.path.abspath(__file__))
-    con.close()
     cur.close()
+    con.close()
+    
 if __name__ == '__main__':
     while True:
         bd_refresh()
