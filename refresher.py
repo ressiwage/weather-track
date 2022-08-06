20088f56d8e6bbe8800af04bb2b97ddff6be2f10
Илья <rsvg@>
2022-08-06 18:48:04 +0500

    deploy

--- refresher.py
+++ refresher.py
@@ -5,24 +5,20 @@ from rq import Worker, Queue, Connection
 
 
 def bd_refresh():
-    con = sqlite3.connect("lab2.db")
+    con = None
+    try:
+        con = sqlite3.connect("lab2.db")
+    except Exception as e:
+        print("ERROR",e)
     cur = con.cursor()
+
     print("iteration")
     data = grab_current()
     sql = "SELECT * FROM `days`"
-    sql = str(f"""
-        INSERT INTO `days`(
-        `id`,
-        `temperature`, 
-        `weather`, 
-        `humidity`,
-        `date`) 
-        VALUES ({random.randint(1_000_000_000, 10_000_000_000)},
-        {data["temperature"]},
-        '{data["weather"]}',
-        {data["humidity"]},
-        '{datetime.datetime.now().replace(microsecond=0)}');
-        """)
+    sql = f"""
+        INSERT INTO days(id, temperature, weather, humidity, date) VALUES(
+        {random.randint(1_000_000_000, 10_000_000_000)}, {data["temperature"]}, '{data["weather"]}', {data["humidity"]}, '{datetime.datetime.now().replace(microsecond=0)}');
+        """
 
     print(sql)
     result = cur.execute(sql)
